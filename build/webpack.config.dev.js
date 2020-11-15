8b05ea431f3bcc601701a596f573fda76c789c47
unknown <kcdrez@gmail.com>
2020-11-15 12:54:42 -0700

    Added header comic font

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -49,10 +49,22 @@ module.exports = {
           }
         } 
       },
-      { 
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
-        loader: 'url-loader?limit=100000' 
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
       }
+      // { 
+      //   test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
+      //   loader: 'url-loader?limit=100000' 
+      // }
     ]
   },  
   plugins: [
